237f88c6029ac7c57d7d473e5bafa698aeaa0c06
ch1ka <164895536+ch1kulya@users.noreply.github.com>
2025-11-01 19:50:49 +0300

    feat: updated readme & added user.js

---
+++ manifest.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name Shiki Watch
+// @namespace https://github.com/ch1kulya/shiki-watch
+// @description The add-on adds the ability to watch on the Shikimori
+// @icon https://raw.github.com/ch1kulya/shiki-watch/main/icons/logo128.png
+// @author ch1kulya
+// @homepageURL https://github.com/ch1kulya/shiki-watch
+// @version 1.0
+// @copyright 2025, ch1kulya
+// @license GPL-3.0-or-later; https://www.gnu.org/licenses/gpl-3.0.txt
+
+// @match *://shikimori.one/*
+
+// @require https://raw.github.com/ch1kulya/shiki-watch/main/scripts/player.js
+
+// @run-at document-start
+
+// ==/UserScript==
