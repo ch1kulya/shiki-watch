c33adbf5c50856010e8acdfb0d3d60d30443e8f4
ch1ka <164895536+ch1kulya@users.noreply.github.com>
2025-11-01 18:49:19 +0300

    chore: cleanup

--- README.md
+++ README.md
@@ -7,40 +7,40 @@ The add-on adds the ability to watch and download anime on the Shikimori.one web
 
 ### Chrome (Chromium)
 1. Install [Tampermonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo) extension
-2. Сlick the button  
+2. Сlick the button
 [![image](https://img.shields.io/static/v1?label=Tampermonkey&message=INSTALL&style=for-the-badge&color=yellow&logo=tampermonkey)](https://github.com/Malanavi/Watch-and-download-on-Shikimori/raw/main/manifest.user.js)
 
 ### Opera (GX)
 1. Install [Violentmonkey](https://chrome.google.com/webstore/detail/violentmonkey/jinjaccalgkegednnccohejagnlnfdag) extension
-2. Сlick the button  
+2. Сlick the button
 [![image](https://img.shields.io/static/v1?label=Violentmonkey&message=INSTALL&style=for-the-badge&color=yellow&logo=tampermonkey)](https://github.com/Malanavi/Watch-and-download-on-Shikimori/raw/main/manifest.user.js)
 
 ### Safari
 1. Install [Userscripts](https://apps.apple.com/app/userscripts/id1463298887) extension
-2. Сlick the button  
+2. Сlick the button
 [![image](https://img.shields.io/static/v1?label=Userscripts&message=INSTALL&style=for-the-badge&color=yellow&logo=safari)](https://github.com/Malanavi/Watch-and-download-on-Shikimori/raw/main/manifest.user.js)
 
 ## Add-on features
-• Shikimori-style "Open Player" button, player, and links  
-•  Kodik and AniLibria player with the ability to select resolution (up to 1080p), playback speed, dubbing, subtitles  
-• The player immediately opens the episode where you left off  
-• Ability to open and close the player  
-• Direct link to search the torrent tracker current anime  
+• Shikimori-style "Open Player" button, player, and links
+•  Kodik and AniLibria player with the ability to select resolution (up to 1080p), playback speed, dubbing, subtitles
+• The player immediately opens the episode where you left off
+• Ability to open and close the player
+• Direct link to search the torrent tracker current anime
 
 ## How to use?
 
-1. Press the button to open the player  
-  
+1. Press the button to open the player
+
 ![image](./images/screenshots/screenshot_1.png?raw=true)
 
-2. Watch anime in the handy kodik player with a huge database of anime, dubbing and subtitles  
-  
+2. Watch anime in the handy kodik player with a huge database of anime, dubbing and subtitles
+
 ![image](./images/screenshots/screenshot_2.png?raw=true)
 
-3. Press the button to close the player  
-  
+3. Press the button to close the player
+
 ![image](./images/screenshots/screenshot_3.png?raw=true)
 
-4. Follow this link to find anime on a torrent tracker (RuTracker) and download  
-  
+4. Follow this link to find anime on a torrent tracker (RuTracker) and download
+
 ![image](./images/screenshots/screenshot_4.png?raw=true)
--- scripts/player.js
+++ scripts/player.js
@@ -1,148 +1,96 @@
-"use strict"
+"use strict";
 
 class Player {
   static {
     console.debug("player loading...");
-
     document.addEventListener("turbolinks:load", () => this.#onViewChanged());
   }
 
-  static playerAdded = false;
   static lang = "rus";
 
   static async #onViewChanged() {
     const match = Shikimori.isAnimePage(window.location);
-    if (match) {
-      if (document.querySelector(".open-player-button")) {
-        document.querySelector(".open-player-button").remove();
-      }
-      if (document.querySelector(".block-with-player")) {
-        document.querySelector(".block-with-player").remove();
-      }
+    if (!match) return;
 
-      this.playerAdded = false;
+    document.querySelector(".block-with-player")?.remove();
 
-      if (document.querySelectorAll(".subheadline")[0].textContent.includes("Information")) {
-        this.lang = "eng"
-      }
-
-      const animeId = match.groups.id;
-
-      const nameOfAnime = Shikimori.getNameOfAnime(animeId);
-
-      const button = this.#createOpenButton(nameOfAnime, animeId);
-
-      const cInfoRight = document.querySelector(".c-info-right");
-      if (cInfoRight) {
-        cInfoRight.appendChild(button);
-      }
-    }
-  }
-
-  static #createOpenButton(nameOfAnime, animeId) {
-    const button = document.createElement("div");
-    button.className = "watch-online open-player-button";
-
-    const line = document.createElement("div");
-    line.className = "line";
-
-    const kind = document.createElement("div");
-    kind.className = "kind";
-    kind.appendChild(document.createTextNode("Kodik / AniLibria"));
-
-    const openPlayer = document.createElement("a");
-    openPlayer.className = "b-link_button dark";
-
-    if (this.lang == "eng") {
-      openPlayer.appendChild(document.createTextNode("Open Player"));
-    }
-    else {
-      openPlayer.appendChild(document.createTextNode("Открыть Плеер"));
+    if (
+      document
+        .querySelectorAll(".subheadline")[0]
+        ?.textContent.includes("Information")
+    ) {
+      this.lang = "eng";
     }
 
-    const style = document.createElement("style");
-    style.textContent = ".p-animes-show .c-info-right .watch-online .b-link_button::after, .p-mangas-show .c-info-right .watch-online .b-link_button::after, .p-ranobe-show .c-info-right .watch-online .b-link_button::after { content: '▶' }";
-    document.head.appendChild(style);
+    const animeId = match.groups.id;
+    const headline = this.#createHeadline();
+    const block = this.#createBlock(headline);
+    const beforeForPlayer = document.getElementsByClassName("b-db_entry")[0];
+    this.#insertAfter(block, beforeForPlayer);
 
-    openPlayer.onclick = () => {
-      if (!this.playerAdded) {
-        const options = this.#createOptions(nameOfAnime, animeId);
-
-        const headline = this.#createHeadline();
-
-        const block = this.#createBlock(options, headline, animeId);
-
-        const beforeForPlayer = document.getElementsByClassName("b-db_entry")[0];
-
-        Helpers.insertAfter(block, beforeForPlayer);
-
-        if (this.lang == "eng") {
-          openPlayer.innerText = "Close Player";
-        }
-        else {
-          openPlayer.innerText = "Закрыть Плеер";
-        }
-
-        style.textContent = ".p-animes-show .c-info-right .watch-online .b-link_button::after, .p-mangas-show .c-info-right .watch-online .b-link_button::after, .p-ranobe-show .c-info-right .watch-online .b-link_button::after { content: '✖' }";
-
-        Kodik.precreateKodikPlayer(animeId);
-
-        this.playerAdded = true;
-      }
-      else if (this.playerAdded) {
-        document.querySelector(".block-with-player").remove();
-
-        if (this.lang == "eng") {
-          openPlayer.innerText = "Open Player";
-        }
-        else {
-          openPlayer.innerText = "Открыть Плеер";
-        }
-
-        style.textContent = ".p-animes-show .c-info-right .watch-online .b-link_button::after, .p-mangas-show .c-info-right .watch-online .b-link_button::after, .p-ranobe-show .c-info-right .watch-online .b-link_button::after { content: '▶' }";
-
-        this.playerAdded = false;
-      }
-    }
-
-    line.appendChild(openPlayer);
-    button.appendChild(line);
-    button.appendChild(kind);
-
-    return button;
-  }
-
-  static #createOptions(nameOfAnime = null, animeId = null) {
-    const options = document.createElement("div");
-    options.className = "b-options-floated mobile-phone";
-
-    options.appendChild(Kodik.createKodikButton(animeId));
-    options.appendChild(AniLibria.createAnilibriaButton(nameOfAnime));
-
-    return options;
+    Kodik.precreateKodikPlayer(animeId);
   }
 
   static #createHeadline() {
     const headline = document.createElement("div");
     headline.className = "subheadline";
-    if (this.lang == "eng") {
-      headline.appendChild(document.createTextNode("watch"));
-    }
-    else {
-      headline.appendChild(document.createTextNode("смотреть"));
-    }
-
+    headline.textContent = this.lang === "eng" ? "watch" : "смотреть";
     return headline;
   }
 
-  static #createBlock(options, headline, animeId) {
+  static #createBlock(headline) {
     const block = document.createElement("div");
     block.className = "block block-with-player";
-
-    block.appendChild(options);
-
     block.appendChild(headline);
-
     return block;
+  }
+
+  static #insertAfter(newNode, existingNode) {
+    existingNode.parentNode.insertBefore(newNode, existingNode.nextSibling);
+  }
+}
+
+class Kodik {
+  static precreateKodikPlayer(animeId) {
+    const block = document.querySelector(".block-with-player");
+    if (!block) return;
+    block.appendChild(this.#createKodikPlayer(animeId));
+  }
+
+  static #createKodikPlayer(animeId) {
+    const kodikPlayer = document.createElement("iframe");
+    kodikPlayer.width = "100%";
+    kodikPlayer.allowFullscreen = true;
+    kodikPlayer.src = `//kodik.cc/find-player?shikimoriID=${animeId}&episode=${Shikimori.getWatchingEpisode(animeId)}`;
+    kodikPlayer.className = "iframe-player";
+
+    new ResizeObserver(() => {
+      kodikPlayer.height = (9 * kodikPlayer.clientWidth) / 16;
+    }).observe(kodikPlayer);
+
+    return kodikPlayer;
+  }
+}
+
+class Shikimori {
+  static isAnimePage(location) {
+    return location.pathname.match(/\/animes\/[a-z]?(?<id>\d+)-[a-z0-9-]+$/);
+  }
+
+  static #fetchAnime(animeId) {
+    const req = new XMLHttpRequest();
+    req.open("GET", `${location.origin}/api/animes/${animeId}`, false);
+    req.send();
+    return JSON.parse(req.response || "{}");
+  }
+
+  static getWatchingEpisode(animeId) {
+    const response = this.#fetchAnime(animeId);
+    return (response.user_rate?.episodes || 0) + 1;
+  }
+
+  static getNameOfAnime(animeId) {
+    const response = this.#fetchAnime(animeId);
+    return response.name || response.english?.[0] || response.russian || "";
   }
 }
