5000467db1c994a577aea5d9a0466d4041c1b35f
ch1ka <164895536+ch1kulya@users.noreply.github.com>
2026-04-10 20:16:30 +0300

    Bump version

--- manifest.user.js
+++ manifest.user.js
@@ -5,7 +5,7 @@
 // @icon https://raw.github.com/ch1kulya/shiki-watch/main/icons/logo128.png
 // @author ch1kulya
 // @homepageURL https://github.com/ch1kulya/shiki-watch
-// @version 1.4
+// @version 1.5
 // @copyright 2026, ch1kulya
 // @license GPL-3.0-or-later; https://www.gnu.org/licenses/gpl-3.0.txt
 
